849c62f086d1df87d514af36d6593e08931ab8eb
RamiHadad1998 <ramihadad125@yahoo.com>
2022-07-27 21:02:03 -0400

    Added test for WholeColumnIsEmpty but I'm not sure if it's needed because it's not any different than the Row test. Also added tests for whole row full of player Q values as well as whole column full of player G values and a separate one for Q values

--- ConnectFour.App/ConnectFour.App.Tests/GameTests.cs
+++ ConnectFour.App/ConnectFour.App.Tests/GameTests.cs
@@ -21,6 +21,16 @@ namespace ConnectFour.App.Tests
         }
 
         [Test]
+        public void IsGameEnded_WholeColumnIsEmpty_False()
+        {
+            var game = new Game();
+
+            var isGameEndedResult = game.IsGameEnded();
+
+            Assert.IsFalse(isGameEndedResult);
+        }
+
+        [Test]
         public void IsGameEnded_WholeRowIsFullOfPlayerGValues_True()
         {
             var game = new Game();
@@ -33,6 +43,63 @@ namespace ConnectFour.App.Tests
             game.MakeMove(new Coordinate(3,5), PlayerMarkerEnum.PlayerG);
             game.MakeMove(new Coordinate(3,6), PlayerMarkerEnum.PlayerG);
             game.MakeMove(new Coordinate(3,7), PlayerMarkerEnum.PlayerG);
+
+            var isGameEndedResult = game.IsGameEnded();
+
+            Assert.IsTrue(isGameEndedResult);
+        } 
+        
+        [Test]
+        public void IsGameEnded_WholeRowIsFullOfPlayerQValues_True()
+        {
+            var game = new Game();
+
+            game.MakeMove(new Coordinate(3,0), PlayerMarkerEnum.PlayerQ);
+            game.MakeMove(new Coordinate(3,1), PlayerMarkerEnum.PlayerQ);
+            game.MakeMove(new Coordinate(3,2), PlayerMarkerEnum.PlayerQ);
+            game.MakeMove(new Coordinate(3,3), PlayerMarkerEnum.PlayerQ);
+            game.MakeMove(new Coordinate(3,4), PlayerMarkerEnum.PlayerQ);
+            game.MakeMove(new Coordinate(3,5), PlayerMarkerEnum.PlayerQ);
+            game.MakeMove(new Coordinate(3,6), PlayerMarkerEnum.PlayerQ);
+            game.MakeMove(new Coordinate(3,7), PlayerMarkerEnum.PlayerQ);
+
+            var isGameEndedResult = game.IsGameEnded();
+
+            Assert.IsTrue(isGameEndedResult);
+        }
+
+        [Test]
+        public void IsGameEnded_WholeColumnIsFullOfPlayerGValues_True()
+        {
+            var game = new Game();
+
+            game.MakeMove(new Coordinate(0,3), PlayerMarkerEnum.PlayerG);
+            game.MakeMove(new Coordinate(1,3), PlayerMarkerEnum.PlayerG);
+            game.MakeMove(new Coordinate(2,3), PlayerMarkerEnum.PlayerG);
+            game.MakeMove(new Coordinate(3,3), PlayerMarkerEnum.PlayerG);
+            game.MakeMove(new Coordinate(4,3), PlayerMarkerEnum.PlayerG);
+            game.MakeMove(new Coordinate(5,3), PlayerMarkerEnum.PlayerG);
+            game.MakeMove(new Coordinate(6,3), PlayerMarkerEnum.PlayerG);
+            game.MakeMove(new Coordinate(7,3), PlayerMarkerEnum.PlayerG);
+
+            var isGameEndedResult = game.IsGameEnded();
+
+            Assert.IsTrue(isGameEndedResult);
+        }
+
+        [Test]
+        public void IsGameEnded_WholeColumnIsFullOfPlayerQValues_True()
+        {
+            var game = new Game();
+
+            game.MakeMove(new Coordinate(0,3), PlayerMarkerEnum.PlayerQ);
+            game.MakeMove(new Coordinate(1,3), PlayerMarkerEnum.PlayerQ);
+            game.MakeMove(new Coordinate(2,3), PlayerMarkerEnum.PlayerQ);
+            game.MakeMove(new Coordinate(3,3), PlayerMarkerEnum.PlayerQ);
+            game.MakeMove(new Coordinate(4,3), PlayerMarkerEnum.PlayerQ);
+            game.MakeMove(new Coordinate(5,3), PlayerMarkerEnum.PlayerQ);
+            game.MakeMove(new Coordinate(6,3), PlayerMarkerEnum.PlayerQ);
+            game.MakeMove(new Coordinate(7,3), PlayerMarkerEnum.PlayerQ);
 
             var isGameEndedResult = game.IsGameEnded();
 
